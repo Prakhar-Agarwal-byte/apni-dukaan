8e1788e42728dc6a18b779f64b6e8898bde01eda
Prakhar <prakhar@everything.net.in>
2021-07-02 17:03:27 +0530

    Added bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,12 @@ import commerce from "../lib/commerce";
 import Link from "next/link";
 
 import ProductList from "../components/ProductList";
+import Appbar from "../components/Appbar";
 
 const HomePage = ({ merchant, categories, products }) => {
   return (
     <>
-      <h1>{merchant.business_name}</h1>
-      <h3>
-        <Link href="/products">
-          <a>Products</a>
-        </Link>
-      </h3>
-
-      <ProductList products={products} />
+      <Appbar />
     </>
   );
 };
